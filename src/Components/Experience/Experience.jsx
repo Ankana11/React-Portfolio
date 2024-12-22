f4b22bd3988f96a936a3a32c80eecac2592de66c
Ankana <reach2ankana@gmail.com>
2024-12-22 22:01:11 +0530

    fix style

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -11,30 +11,31 @@ function Experience() {
         <h2>My <span className="highlight">Experiences</span></h2>
       </div>
         <div className='main'>
-        <div className='timeline'>
-            <div className='container left-container'>
-              
-                <img src={lyft} className="circle" alt=""/>
-                <div className='text-box htmlcard'>
-                <div className="blur blur-1"></div>
-                    <h2 className='htmltext'>Lyft</h2>
-                    <small>2023 - 2024</small>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, dolorem.</p>
-                    <span className='left-container-arrow'></span>
-                </div>
-            </div>
-            <div className='container right-container'>
-               
-                <img src={playbees} className="circle" alt='' />
-                <div className="blur blur-2"></div>
-                <div className='text-box javascriptcard'>
-                    <h2 className='javascripttext'>Playbess Technologies</h2>
-                    <small>2023 - 2024</small>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, dolorem.</p>
-                    <span className='right-container-arrow'></span>
-                </div>
+        <div className="timeline-wrapper">
+    <div className='timeline'>
+       
+        <div className='container left-container'>
+            <img src={lyft} className="circle" alt="Lyft Logo" />
+            <div className='text-box'>
+                <h2>Lyft</h2>
+                <small>2023 - 2024</small>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, dolorem.</p>
+                <span className='left-container-arrow'></span>
             </div>
         </div>
+       
+        <div className='container right-container'>
+            <img src={playbees} className="circle" alt="Playbess Logo" />
+            <div className='text-box'>
+                <h2>Playbess Technologies</h2>
+                <small>2023 - 2024</small>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, dolorem.</p>
+                <span className='right-container-arrow'></span>
+            </div>
+        </div>
+    </div>
+</div>
+
     </div>
     </div>
   )
