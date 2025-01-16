ee98a3f26297119976d6e7303ee490c4997fe174
Ankana <reach2ankana@gmail.com>
2025-01-16 21:25:01 +0530

    experinece

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -2,15 +2,20 @@ import React from "react";
 import "./Experience.css";
 import lyft from "../../img/lyft.png";
 import playbees from "../../img/playbees.png";
+import me from "../../img/final_exp.png";
 function Experience() {
+    
   return (
     <div className="experience-container">
                
        
-              <div className="ex-header ex">
+               <div className="ex-header ex">
+        <img src={me} alt="Me" className="me-image" />
         <h2>My <span className="highlight">Experiences</span></h2>
-      </div>
-    
+        <div className="blur t-blur1" style={{ background: "var(--purple)" }}></div>
+        <div className="blur t-blur2" style={{ background: "skyblue" }}></div>
+        </div>
+            
         <div className="timeline-wrapper">
        
     <div className='timeline'>
@@ -31,11 +36,11 @@ function Experience() {
             <img src={playbees} className="circle" alt="Playbess Logo" />
             <div className='text-box'>
                 <h4>Playbess Technologies</h4>
-                <small>2023 - 2024</small>
+                <small>2024 - Present</small>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, dolorem.</p>
                 <span className='right-container-arrow'></span>
             </div>
-        <div className="blur blur-1"></div>
+        
         </div>
     </div>
 </div>
