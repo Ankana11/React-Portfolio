d197c62461bfb540a76a74795f6586dc94ea5428
Ankana <reach2ankana@gmail.com>
2025-01-25 21:40:14 +0530

    fix

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -7,18 +7,17 @@ import girl1 from "../../img/girl1.png";
 import glassesimoji from "../../img/glassesimoji.png";
 import thumbup from "../../img/thumbup.png";
 
-
 import code from "../../img/code.png";
 import FloatinDiv from "../FloatingDiv/FloatingDiv";
 import Github from "../../img/github.png";
 import LinkedIn from "../../img/linkedin.png";
 import Instagram from "../../img/instagram.png";
 import { themeContext } from "../../Context";
-// import { motion } from "framer-motion";
+import { motion } from "framer-motion";
 import { Link } from "react-scroll";
 const Intro = () => {
   // Transition
-  // const transition = { duration: 2, type: "spring" };
+  const transition = { duration: 2, type: "spring" };
 
   // context
   const theme = useContext(themeContext);
@@ -28,13 +27,8 @@ const Intro = () => {
     <div className="Intro" id="Intro">
       {/* left name side */}
       <div className="blur" style={{
-        // background: "rgba(255, 179, 71, 0.6)",
- 
   background: "rgba(255, 209, 220, 0.6)",
-  // background: "#e9ff0f5c",
   top: "-2rem",
-  // width: "21rem",
-  // height: "11rem",
   left: "-9rem",
 
         
@@ -44,9 +38,9 @@ const Intro = () => {
           {/* yahan change hy darkmode ka */}
           <span style={{ color: darkMode ? "white" : "" }}>Hey! I Am</span>
           <span>Ankana Das</span>
-          <span style={{ color: darkMode ? "white" : "" }}>A Design Centric Software Developer</span>
+          <span style={{ color: darkMode ? "white" : ""}}>A Design Centric Software Developer</span>
         </div>
-        <Link to="contact" smooth={true} spy={true}>
+        <Link to="about" smooth={true} spy={true}>
           <button className="button i-button">Know me</button>
         </Link>
         {/* social icons */}
@@ -62,14 +56,14 @@ const Intro = () => {
         <img src={Vector2} alt="" />
         <img src={girl1} alt="" />
         
-        <img src={glassesimoji} alt="" />
-         {/* <motion.img
+        {/* <img src={glassesimoji} alt="" /> */}
+         <motion.img
           initial={{ left: "-36%" }}
           whileInView={{ left: "-24%" }}
           transition={transition}
           src={glassesimoji}
           alt=""
-        /> */}
+        />
         <div style={{ position: "absolute", top: "-2%", left: "62%" }}>
   <FloatinDiv
     img={code}
